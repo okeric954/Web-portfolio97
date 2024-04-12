7e4356c70f7d6afacb1bea1e31ca7db15799775f
Eric <165939664+okeric954@users.noreply.github.com>
2024-04-12 05:19:11 +0000

    Beginning update

--- project/app.js
+++ project/app.js
@@ -1,2 +1,18 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+// Define port and use 3000 as default
+const port = process.env.PORT || 3000;
+// Configure static directory for serving CSS, JS, images, etc.
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "src/views")));
+app.use(express.static(path.join(__dirname, "bower_components")));
+
+// Define routes
+app.get("/", (req, res) => {
+  res.send("Hello Friends!");
+});
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
